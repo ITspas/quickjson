2ea37e841a008fd3049f066d525207e8e9b576f1
unknown <Netxy@vip.qq.com>
2015-02-07 03:28:21 +0800

    quickjson

---
+++ README.md
@@ -0,0 +1,38 @@
+QuickJson
+---------
+Java 操作json框架
+
+```Java
+public class QuickJsonTest {
+
+	@Test
+	public void test1() {
+		JSONObject jsonObject = (JSONObject) JSON.parse("{\"array\":[1,2,3],\"boolean\":true,\"null\":null,\"number\":123,\"object\":{\"a\":\"b\",\"c\":\"d\",\"e\":\"f\"},\"string\":\"Hello World\"}");
+		System.out.println(jsonObject.getStringValue("string"));
+		System.out.println(jsonObject.getJSONObject("string"));
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println(JSON.parse("{\"name\":\"Tom\",\"age\":10,\"address\":\"anhui hfei\"}", A.class));
+		System.out.println(JSON.parseJSONObject("{\"name\":\"Tom\",\"age\":10,\"address\":\"anhui hfei\"}").toPojo(A.class));
+		System.out.println(JSON.parseJSONArray("[{\"name\":\"Tom\",\"age\":10,\"address\":\"anhui hfei\"}]").getPojoValue(0, A.class));
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println(JSON.toJSONString(new A("Tom", 11, "anhui hefei")));
+		System.out.println(JSON.toJSONString(new int[]{10,10,102,200}));
+		HashMap<String, Object> map = new HashMap<String,Object>();
+		map.put("name", "Tom");
+		map.put("age",11);
+		map.put("address", "anhui hefei");
+		System.out.println(JSON.toJSONString(map));
+		List<String> list = new ArrayList<String>();
+		list.add("Tom");
+		list.add("jack");
+		System.out.println(JSON.toJSONString(list));
+	}
+}
+
+```
---
+++ src/main/java/org/itspas/quickjson/JSON.java
@@ -0,0 +1,195 @@
+package org.itspas.quickjson;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class JSON {
+	public static Object parse(String text) {
+		return isEmtry(text) ? null : parseJSON(text);
+	}
+
+	public static <T extends Object> T parse(String text, Class<T> clazz) {
+		if (null == text || text.isEmpty() || null == clazz || clazz.isInterface() || !isJSONObject(text))
+			return null;
+		return CaseUtils.objectValue(parseJSONObject(text), clazz);
+	}
+
+	public static JSONArray parseJSONArray(String text) {
+		return (isEmtry(text) || !isJSONArray(text)) ? null : JSONArray.class.cast(parseJSON(text));
+	}
+
+	public static JSONObject parseJSONObject(String text) {
+		return (isEmtry(text) || !isJSONObject(text)) ? null : JSONObject.class.cast(parseJSON(text));
+	}
+
+	public static String toJSONString(Object object) {
+		if (null == object)
+			return null;
+		if (CaseUtils.isPrimitive(object.getClass())) {
+			return String.class.isInstance(object) ? String.format("\"%s\"", object.toString()) : object.toString();
+		} else if (object.getClass().isArray() || Collection.class.isInstance(object)) {
+			return toJSONStringArray(object);
+		} else if (Map.class.isInstance(object)) {
+			return toJSONStringMap(object);
+		} else {
+			return toJSONStringPojo(object);
+		}
+	}
+
+	private static String toJSONStringMap(Object object) {
+		StringBuilder builder = new StringBuilder("{");
+		Map<?, ?> map = Map.class.cast(object);
+		Iterator<?> iterator = map.keySet().iterator();
+		while (iterator.hasNext()) {
+			Object k = iterator.next();
+			if (CaseUtils.isPrimitive(k.getClass())) {
+				Object v = map.get(k);
+				String newK = toJSONString(k);
+				builder.append(String.format("%s:%s,", newK.startsWith("\"") ? newK : String.format("\"%s\"", newK), toJSONString(v)));
+			}
+		}
+		if (builder.charAt(builder.length() - 1) == ',')
+			builder.setLength(builder.length() - 1);
+		builder.append("}");
+		return builder.toString();
+	}
+
+	private static String toJSONStringArray(Object object) {
+		if (Collection.class.isInstance(object))
+			object = List.class.cast(object).toArray();
+		int count = Array.getLength(object);
+		StringBuilder builder = new StringBuilder("[");
+		for (int i = 0; i < count; i++) {
+			builder.append(String.format("%s,", toJSONString(Array.get(object, i))));
+		}
+		if (builder.charAt(builder.length() - 1) == ',')
+			builder.setLength(builder.length() - 1);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	private static String toJSONStringPojo(Object object) {
+		StringBuilder builder = new StringBuilder("{");
+		Method[] methods = object.getClass().getMethods();
+		for (Method method : methods) {
+			if (!method.getName().startsWith("get") || method.getParameterTypes().length > 0 || method.getName().equals("getClass"))
+				continue;
+			String name = String.format("%s%s", Character.toLowerCase(method.getName().charAt(3)), method.getName().substring(4));
+			try {
+				Object result = method.invoke(object);
+				if (null != result) {
+					builder.append(String.format("\"%s\":%s,", name, toJSONString(result)));
+				}
+			} catch (Exception e) {
+				continue;
+			}
+		}
+		if (builder.charAt(builder.length() - 1) == ',')
+			builder.setLength(builder.length() - 1);
+		builder.append("}");
+		return builder.toString();
+	}
+
+	private static boolean isJSONObject(String text) {
+		text = text.trim();
+		return text.startsWith("{") && text.endsWith("}");
+	}
+
+	private static boolean isJSONArray(String text) {
+		text = text.trim();
+		return text.startsWith("[") && text.endsWith("]");
+	}
+
+	private static boolean isEmtry(String text) {
+		return null == text || text.isEmpty();
+	}
+
+	private static Object parseJSON(String text) {
+		text.trim();
+		Stack<String> stack = new Stack<String>();
+		Stack<Object> objects = new Stack<Object>();
+		Stack<String> keys = new Stack<String>();
+		Stack<Boolean> types = new Stack<Boolean>();
+		int idx = 0;
+		int pos = 0;
+		Object obj = null;
+		do {
+			switch (text.charAt(idx++)) {
+			case '[':
+			case '{':
+				boolean isMap = text.charAt(idx - 1) == '{';
+				if (stack.size() != 0 && stack.lastElement().equals("\"")) {
+					break;
+				}
+				stack.push("}");
+				types.push(isMap);
+				if (null != obj)
+					objects.push(obj);
+				obj = obtain(isMap ? JSONObject.class : JSONArray.class);
+				pos = idx;
+				break;
+			case ']':
+			case '}':
+				if (pos != idx) {
+					String value = text.substring(pos, idx - (text.charAt(idx - 2) == '"' ? 2 : 1)).trim();
+					if (obj instanceof HashMap) {
+						JSONObject.class.cast(obj).put(keys.pop(), value);
+					} else {
+						JSONArray.class.cast(obj).add(value);
+					}
+				}
+				types.pop();
+				stack.pop();
+				if (objects.size() > 0) {
+					if (objects.lastElement() instanceof HashMap) {
+						JSONObject.class.cast(objects.lastElement()).put(keys.pop(), obj);
+					} else {
+						JSONArray.class.cast(objects.lastElement()).add(obj);
+					}
+					obj = objects.pop();
+				}
+				pos = idx + 1;
+				break;
+			case '"':
+				if (stack.lastElement().equals("\"")) {
+					stack.pop();
+				} else {
+					stack.push("\"");
+					pos = idx;
+				}
+				break;
+			case ',':
+				if (pos != idx) {
+					String value = text.substring(pos, idx - (text.charAt(idx - 2) == '"' ? 2 : 1)).trim();
+					if (types.lastElement()) {
+						JSONObject.class.cast(obj).put(keys.pop(), value);
+					} else {
+						JSONArray.class.cast(obj).add(value);
+					}
+				}
+				pos = idx;
+				break;
+			case ':':
+				keys.push(text.substring(pos, (pos = idx) - 2));
+				break;
+			}
+		} while (idx < text.length());
+		return obj;
+	}
+
+	private static <T extends Object> T obtain(Class<T> clazz) {
+		try {
+			return clazz.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
